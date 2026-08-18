175499c036eb340f694fa7f83599cdee1d1f725a
Mallika <mallika.chakraborty@siemens.com>
2026-08-18 10:30:54 -0400

    feat: add header-test global script

---
+++ scripts/header-test/script.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  function log(msg) {
+    console.log('[header-test]', msg);
+  }
+
+  function loadScript(src, onLoad, onError) {
+    var s = document.createElement('script');
+    s.src = src;
+    s.onload = onLoad;
+    s.onerror = onError;
+    document.head.appendChild(s);
+  }
+
+  loadScript(
+    'https://static.sw.cdn.siemens.com/xd/xd-utils/next/xd-utils.min.js',
+    function () {
+      log('xd-utils loaded OK');
+      if (window.disw) {
+        window.disw.init({ account: { authEnv: 'dev' }, locales: true });
+        log('disw.init called OK');
+      } else {
+        log('ERROR: window.disw not found after xd-utils load');
+      }
+
+      loadScript(
+        'https://static.sw.cdn.siemens.com/xd/universal-components/next/web/index.js',
+        function () {
+          log('universal-components loaded OK');
+          if (window.universalComponents) {
+            window.universalComponents.init(['header-xd-v1']);
+            log('universalComponents.init called OK');
+          } else {
+            log('ERROR: window.universalComponents not found');
+          }
+        },
+        function () {
+          log('ERROR: failed to load universal-components from CDN');
+        }
+      );
+    },
+    function () {
+      log('ERROR: failed to load xd-utils from CDN');
+    }
+  );
+})();
